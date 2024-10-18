05a679eb7f9c9178b53ee0c89690e17831ea1431
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-18 10:39:04 +0900

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { skillsData } from './data/skill';
+import './App.css';
 
 function App() {
+  
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="intro">
+        <h1>끊임없이 고민하고 성장하는 개발자 박현준입니다</h1>
+        <p>Java/Spring을 활용한 백엔드 개발을 전공으로 하고 있으며 Docker, Kubernetes와 같은 DevOps를 공부하고 있습니다</p>
+      </div>
+      <div className="skills">
+        <div className="skills-grid">
+          {skillsData.map((skill, index) => (
+            <div className="skill-card" key={index}>
+              <img src={skill.img} alt={skill.name} className="skill-icon" />
+              <p>{skill.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="projects">
+        <h2>프로젝트</h2>
+        <div className="project">
+          <h3>AriPay</h3>
+          <p>학교 매점 결제를 간편화하고 재고관리를 도와주는 ERP 서비스입니다</p>
+          <a href="https://occount.bsm-aripay.kr/">My Portfolio Link</a>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/data/skill.js
@@ -0,0 +1,13 @@
+import javaLogo from '../assets/java.png';
+import springLogo from '../assets/spring.png';
+import jsLogo from '../assets/javascript.png';
+import nodeLogo from '../assets/nodejs.png';
+import flutterLogo from '../assets/flutter.png';
+
+export const skillsData = [
+    { name: 'Java', img: javaLogo },
+    { name: 'Spring', img: springLogo },
+    { name: 'JavaScript', img: jsLogo },
+    { name: 'NodeJS', img: nodeLogo },
+    { name: 'Flutter', img: flutterLogo },
+];
